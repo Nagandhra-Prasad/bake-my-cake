fecf6a43691a57951dfbc38beb92affbf457b1c6
ethanwinters05 <147235231+ethanwinters05@users.noreply.github.com>
2024-03-31 14:08:15 +0530

    initial commit

---
+++ bake-my-cake/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { OrderViewComponent } from './order-view/order-view.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductRequestComponent } from './product-request/product-request.component';
+import { authGuard } from './services/auth.guard';
+import { canDeactivateGuard } from './services/can-deactivate.guard';
+import { CustomComponent } from './custom/custom.component';
+
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", component: ProductViewComponent },
+  {path:"customization",component:CustomComponent},
+  { path: "order-view/:id", component: OrderViewComponent, canDeactivate: [canDeactivateGuard] },
+
+  { path: "product-requests", component: ProductRequestComponent, canActivate: [authGuard] },
+  
+  { path: "**", component: PageNotFoundComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+
+
+
+
---
+++ bake-my-cake/src/app/custom/custom.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProductService } from '../services/product.service';
+import { ProductRequestService } from '../services/product-request.service';
+import { ActivatedRoute } from '@angular/router';
+import { RouteService } from '../services/route.service';
+import { ProductRequest } from '../models/product-request';
+import { Product } from '../models/product';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-custom',
+  templateUrl: './custom.component.html',
+  styleUrls: ['./custom.component.css']
+})
+export class CustomComponent implements OnInit {
+
+ 
+  minDate = new Date();
+  constructor(private productService: ProductService,
+    private productRequestService: ProductRequestService,
+    private snackBar: MatSnackBar,
+    private activatedRoute: ActivatedRoute,
+    private routeService: RouteService) { 
+      this.minDate.setDate(this.minDate.getDate())
+    }
+  product!: Product
+  productRequest: ProductRequest = {
+    id: 0,
+    itemName: '',
+    quantity: '',
+    price: 0,
+    dateOfOrder: new Date(),
+    customerName: '',
+    customerEmail: '',
+    customerPhone: '',
+    citation: '',
+    doorNo: '',
+    street: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    totalAmount: 0
+  }
+
+
+  
+
+  // totalAmount = 0
+
+
+  submitStatus: boolean = false
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(param => {
+      let id = param.get("id") ?? 0;
+      this.productService.getproduct(+id).subscribe(data => {
+        this.product = data;
+
+      })
+    })
+
+  }
+
+
+
+  makeRequest() {
+    if (this.productRequest.customerName && this.productRequest.customerEmail && this.productRequest.customerPhone && this.productRequest.dateOfOrder) {
+      this.productRequest.itemName = this.product?.itemName;
+      this.productRequest.price = this.product?.price;
+
+      this.productRequestService.saveProductRequest(this.productRequest).subscribe
+        ({
+          next: data => {
+            this.snackBar.open("Request Submitted", "Success", { duration: 3000 });
+            this.submitStatus = true;
+            this.routeService.navigateToHomeView();
+          },
+          error: err => {
+            console.log(err);
+            alert(err);
+          }
+        })
+    }
+  }
+
+
+  calculateTotalPrice() {
+    let quantity1 = this.productRequest.quantity;
+    if (this.product.price != undefined && quantity1 >= 0) {
+      this.productRequest.totalAmount = quantity1 * this.product.price
+    }
+    else {
+      this.productRequest.quantity = 0
+    }
+  }
+
+
+  canDeactivate() {
+    if (!this.submitStatus) {
+      this.submitStatus = confirm("Are you sure You  want to leave the page")
+      return this.submitStatus
+    }
+    return true
+  } 
+
+}
